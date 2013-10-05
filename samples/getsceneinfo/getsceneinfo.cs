2a81e1455c1bd4284d326bac234b75f6727f3aea
Takuya Murakita <murakita@3dp.co.jp>
2013-10-05 11:42:20 +0900

    Connection settings were changed from hard-code to xml.

--- samples/getsceneinfo/getsceneinfo.cs
+++ samples/getsceneinfo/getsceneinfo.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using mujincontrollerclient;
+using System.Xml;
 
 namespace getsceneinfo
 {
@@ -11,18 +12,24 @@ namespace getsceneinfo
     {
         static void Main(string[] args)
         {
-            string mujinIpAddress = "http://192.168.11.12:80/";
-            string scenePrimaryKey = "irex2013.mujin.dae";
-            string username = "testuser";
-            string password = "pass";
-            string taskName = "testTask001";
-            string taskType = "binpicking";
-            string controllerip = "192.168.11.110";
-            int controllerport = 5008;
+            string xmlFilepath = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "..\\..\\samples\\getsceneinfo\\config\\connection.xml";
+
+            XmlDocument document = new XmlDocument();
+            StreamReader reader = new StreamReader(xmlFilepath, Encoding.UTF8);
+            document.Load(reader);
+
+            string mujinIpAddress   = document.GetElementsByTagName("ipaddress")[0].InnerText;
+            string scenePrimaryKey  = document.GetElementsByTagName("scenepk")[0].InnerText;
+            string username         = document.GetElementsByTagName("username")[0].InnerText;
+            string password         = document.GetElementsByTagName("password")[0].InnerText;
+            string taskName         = document.GetElementsByTagName("taskname")[0].InnerText;
+            string taskType         = document.GetElementsByTagName("tasktype")[0].InnerText;
+            string controllerip     = document.GetElementsByTagName("controllerip")[0].InnerText;
+            int controllerport      = int.Parse(document.GetElementsByTagName("controllerport")[0].InnerText);
 
             ControllerClient controllerClient = new ControllerClient(username, password, mujinIpAddress);
 
-            controllerClient.Initialize("mujin:/irex2013/irex2013.WPJ", "mujincollada", "wincaps", "mujin:/irex2013.muin.dae");
+            //controllerClient.Initialize("mujin:/irex2013/irex2013.WPJ", "mujincollada", "wincaps", "mujin:/irex2013.muin.dae");
             
             Scene scene = controllerClient.GetScene(scenePrimaryKey);
             
