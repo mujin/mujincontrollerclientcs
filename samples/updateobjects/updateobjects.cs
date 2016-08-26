09a7cd557b2e8aed64a5a6d16c999b607418cfef
Ziyan Zhou <ziyan.zhou@mujin.co.jp>
2016-08-26 15:13:56 +0900

    Update sample.

--- samples/updateobjects/updateobjects.cs
+++ samples/updateobjects/updateobjects.cs
@@ -13,17 +13,18 @@ namespace updateobjects
     {
         static void Main(string[] args)
         {
-            string hostname = "controller32";
-            ControllerClient controllerClient = new ControllerClient("testuser", "pass", "http://" + hostname);
+            string hostname = "controller65";
+            string username = "testuser";
+            string password = "pass";
+            string scenepk = "knowledgepicking1.mujin.dae";
 
-            SceneResource scene = controllerClient.GetScene("metalringntnpicking4.mujin.dae");
-            BinPickingTask task = (BinPickingTask)scene.GetOrCreateTaskFromName("test", "binpicking");
+            ControllerClient controllerClient = new ControllerClient(username, password, "http://" + hostname);
+
+            SceneResource scene = controllerClient.GetScene(scenepk);
+            BinPickingTask task = (BinPickingTask)scene.GetOrCreateTaskFromName("test_task_1", "binpicking");
 
             // slave request id for the target slave, usually hostname + "_slave0"
             task.SlaveRequestID = hostname + "_slave0";
-
-            // uri to the object model file
-            string objecturi = "mujin:/metalring_ntn2.mujin.dae";
 
             // remove objects in the scene whose names have this prefix, optional
             string prefix = "testobject_";
@@ -34,17 +35,17 @@ namespace updateobjects
                     { "name", prefix + "1" },
                     { "quat_", new List<double>() { 1, 0, 0, 0 } },
                     { "translation_", new List<double>() { 0, 0, 300 }  },
-                    { "object_uri", objecturi },
+                    { "object_uri", "mujin:/work1.mujin.dae" },
                 },
                 new Dictionary<string, object>() {
                     { "name", prefix + "2" },
                     { "quat_", new List<double>() { 1, 0, 0, 0 } },
                     { "translation_", new List<double>() { 0, 100, 400 }  },
-                    { "object_uri", objecturi },
+                    { "object_uri", "mujin:/work2_b.mujin.dae" },
                 }
             };
 
-            task.UpdateObjects(envstate, prefix, objecturi);
+            task.UpdateObjects(envstate, prefix);
         }
     }
 }
